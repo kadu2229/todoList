b261cbaf29016a5250e2f79e3aeae408ff667660
Carlos <kadu2229@yahoo.com.br>
2025-07-18 00:54:25 -0300

    integração com banco de dados e funcionalidade de cadastrar as tarefas no banco

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const express = require('express');
 const hndbrs = require('express-handlebars');
 const app = express()
 const conection = require('./db/conection')
+const tarefas = require('./modules/tarefas')
 
 app.engine('handlebars', hndbrs.engine())
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.post('/addTask', async (req, res) => {
+  const { task, description } = req.body
+
+  await tarefas.create({task, description})
+
+  res.redirect('/')
 })
 
 try {
