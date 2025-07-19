f334df114e6d95ec7ad1aa95abd1f715eaa76acc
gabrielaguedes12 <git@github.com:gabrielaguedes12/curso-front-end.git>
2025-07-19 00:22:53 -0300

    estrutura todolist

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hndbrs = require('express-handlebars');
 const app = express()
-const conection = require('./db/conection')
+//const conection = require('./db/conection')
 
 app.engine('handlebars', hndbrs.engine())
 app.set('view engine', 'handlebars')
@@ -12,11 +12,13 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-try {
+  app.listen(3000)
+
+/*try {
   conection.sync().then(() => {
     app.listen(3000)
   })
   console.log('aplicação rodando na porta 3000')
 } catch(err) {
   console.log
-}
+}*/
