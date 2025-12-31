12dca17613e038c1f62aa78dee6440b06bb79299
Carlos <kadu2229@yahoo.com.br>
2025-12-31 20:36:23 -0300

    criação de model, primeira view e primeira parte do crud(salvar dados implementada)

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const conection = require("./DataBase/conection");
 const hndbrs = require("express-handlebars");
+const path = require("path");
+const tasks = require('./models/tasks')
 
 const app = express();
 app.engine("handlebars", hndbrs.engine());
 app.set("view engine", "handlebars");
-
-app.get("/", (req, res) => {
-  res.render("home");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json())
+app.get("/", async (req, res) => {
+  const data = await tasks.findAll({raw: true})
+  res.render("home", {data});
 });
+
+app.post('/addTask', async (req,res) => {
+  const { task,description } = req.body;
+  console.log(data)
+  await tasks.create({
+    task,
+    description
+  })
+  res.redirect('/')
+})
+
+app.post('/deleteTask', async (req, res) => {
+  const data = await tasks.findAll({raw: true})
+
+
+})
 
 const appInit = async () => {
   try {
--- models/tasks.js
+++ models/tasks.js
@@ -1,12 +1,15 @@
 const { DataTypes } = require('sequelize');
 const conection = require('../DataBase/conection');
 
-const task = conection.define(task, {
+const tasks = conection.define('tasks', {
   task: {
     type: DataTypes.STRING,
     allowNull: false
   },
   description: {
-    
+    type: DataTypes.TEXT,
+    allowNull: false
   }
 })
+
+module.exports = tasks;
